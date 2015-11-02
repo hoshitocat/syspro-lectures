6053f70d5cc5d2bc917bd47ec3748c409b461397
dor0cy <hoshitocat@gmail.com>
2015-11-02 14:38:20 +0900

    added ex3

---
+++ ex3/netlib.h
@@ -0,0 +1,19 @@
+#ifndef NETLIB_H
+
+#define NETLIB_H
+
+#include <arpa/inet.h>
+#include <stdio.h>
+
+int make_server_sock( int port );
+int connect_dest( char *host_name, char *port );
+
+#ifdef DEBUG
+void show_addr( const char *msg, struct sockaddr_in *addr );
+#else
+#define show_addr(msg, addr)
+#endif
+
+#define check(e, msg, erroval) do { if ( (e) < 0 ) { perror(msg); return (erroval); } } while(0)
+
+#endif
