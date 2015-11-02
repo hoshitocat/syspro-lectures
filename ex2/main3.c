019145ee3616d83b44f8db89e8b34605e529e39b
dor0cy <hoshitocat@gmail.com>
2015-11-02 11:35:13 +0900

    added testcase1 for ex2

--- ex2/main3.c
+++ ex2/main3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <math.h>
 
@@ -20,16 +21,52 @@ void testcase4(void *mem_table[]);
 int main() {
   void *mem_table[1000];
 
-  // testcase1();
+  testcase1(mem_table);
   testcase2(mem_table);
-  // testcase3();
-  // testcase4();
+  testcase3();
+  testcase4(mem_table);
 
   return 0;
 }
 
 /* LIFO順でない割り付け・解放を多数回繰り返しても失敗しない． */
 void testcase1(void *mem_table[]) {
+  int num_of_trials = 1000;
+  int current_p;
+  int i = 0;
+
+  printf(" Test Case 1: ");
+  // NOTE: 乱数のseedの初期化
+  srand((unsigned)time(NULL));
+
+  // 最初適当数割り付けを行う
+  for ( i = 0; i < 1000; i++ ) {
+    mem_table[i] = alloc3( 1000 );
+    if ( mem_table[i] == NULL ) { printf("failed memory allocation !!\n"); }
+  }
+
+  for ( i = 0; i < num_of_trials; i++) {
+    current_p = rand() % 1000;
+    if ( mem_table[current_p] == NULL ) {
+      mem_table[current_p] = alloc3( 1000 );
+      if ( mem_table[current_p] == NULL ) { printf("failed memory allocation !!\n"); }
+    } else {
+      afree3( mem_table[current_p] );
+      mem_table[current_p] = NULL;
+    }
+  }
+
+  // 最後mem_tableを元に戻す
+  for ( i = 0; i < 1000; i++ ) {
+    if ( mem_table[i] != NULL ) {
+      afree3( mem_table[i] );
+      mem_table[i] = NULL;
+    }
+  }
+
+  printf("successed :) \n");
+
+  return;
 }
 
 /* 合計で数MB程度の領域を割り付けてもエラーにならない． */
@@ -39,7 +76,7 @@ void testcase2(void *mem_table[]) {
   int maximum_alloc = pow(1024, 2); // 割り付けテストを行う最大値
   int i;
 
-  printf(" Test Case 1: ");
+  printf(" Test Case 2: ");
   // NOTE: 乱数のseedの初期化
   srand((unsigned)time(NULL));
 
